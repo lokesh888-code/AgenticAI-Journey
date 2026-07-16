dc08a7f80f32eea99f7fc8275dd67bbafebcb214
lokesh888-code <156835486+lokesh888-code@users.noreply.github.com>
2026-07-16 18:58:36 +0530

    Add files via upload

---
+++ 01_Python_Fundamentals/Day10/file_write.py
@@ -0,0 +1,6 @@
+with open("students.txt","w") as file:
+    file.write("Ramu")
+    file.write("\nLokesh")
+    file.write("\nYeswanth")
+file.close()
+
---
+++ 01_Python_Fundamentals/Day10/logger_intro.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("Application started")
+logging.warning("Sample warning")
+logging.error("Sample error")
